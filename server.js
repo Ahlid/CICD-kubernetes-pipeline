f169dbec577740e9cd9c7019b736f2247f67f072
Ricardo Morais <moraispgsi@gmail.com>
2017-12-31 10:20:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-var employeeProvider= new EmployeeProvider('mongo-0.mongo,mongo-1.mongo,mongo-2.mongo', 27017);
+var employeeProvider = new EmployeeProvider();
 
 //Routes
 
